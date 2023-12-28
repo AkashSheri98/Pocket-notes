d5527b14c84b2feadb5766df0db1c3ab4bd9fb22
AkashSheri98 <akashsheri2367@gmail.com>
2023-12-28 22:48:57 +0530

    deploye 4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pocket-notes/",
-  plugins: [react()]
-  
+
+  plugins: [react()],
+  base: "/Pocket-notes/"
 })
